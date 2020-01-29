ee77bbc1e473944d0b1f5730a4bc53dab5d5fea8
taoting <335354289@qq.com>
2020-01-30 00:42:28 +0800

    完善task

--- task/task.py
+++ task/task.py
@@ -2,6 +2,8 @@ from celery import Celery
 
 from app import create_app
 
+from app.libs.spider_service import crawl_accept_problem, task_crawl_accept_problem
+
 celery = Celery('tasks')
 celery.config_from_object('app.config.setting')
 celery.config_from_object('app.config.secure')
@@ -11,7 +13,7 @@ celery.config_from_object('app.config.secure')
 def setup_periodic_tasks(sender, **kwargs):
     sender.add_periodic_task(3600, name='1111',
                              sig=task_f,
-                             args=[])
+                             args=[task_crawl_accept_problem])
 
 
 @celery.task
