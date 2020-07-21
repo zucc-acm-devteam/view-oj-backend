f3754aefc75e39907eac73ecdea16c9b010a8052
jiubei0408 <757795210@qq.com>
2020-07-21 20:13:58 +0800

    删除oj_username时删掉做题记录

--- app/models/camp_models/course_oj_username.py
+++ app/models/camp_models/course_oj_username.py
@@ -23,6 +23,10 @@ class CourseOJUsername(Base):
         return CampOJ.get_by_id(self.oj_id)
 
     @property
+    def course(self):
+        return Course.get_by_id(self.course_id)
+
+    @property
     def rating(self):
         from app.models.camp_models.course import Course
         from app.models.camp_models.user_contest import UserContest
@@ -44,3 +48,20 @@ class CourseOJUsername(Base):
             return r[0]
         else:
             return None
+
+    def delete(self):
+        from app.models.camp_models.user_contest import UserContest
+        from app.models.camp_models.camp_accept_problem import CampAcceptProblem
+        contests = self.course.contests
+        for contest in contests:
+            UserContest.get_by_username_and_contest_id(
+                self.username,
+                contest.id
+            ).delete()
+            accept_problems = CampAcceptProblem.search(
+                username=self.username,
+                contest_id=contest.id
+            )['data']
+            for accept_problem in accept_problems:
+                accept_problem.delete()
+        super().delete()
