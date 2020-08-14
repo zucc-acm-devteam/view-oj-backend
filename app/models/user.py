aa5d505119d2ebc3b539bdb070461a4f97347195
jiubei0408 <757795210@qq.com>
2020-08-14 19:13:51 +0800

    名字颜色后端功能加强

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,7 @@ from app.models.base import Base, db
 class User(UserMixin, Base):
     __tablename__ = 'user'
 
-    fields = ['username', 'nickname', 'group', 'permission', 'status', 'name_color', 'name_back_color']
+    fields = ['username', 'nickname', 'group', 'permission', 'status', 'custom_color']
 
     username = Column(String(100), primary_key=True)
     nickname = Column(String(100), nullable=False)
@@ -20,8 +20,7 @@ class User(UserMixin, Base):
     rating = Column(Integer, nullable=False, default=0)
     codeforces_rating = Column(Integer, nullable=False, default=0)
     contest_num = Column(Integer, nullable=False, default=0)
-    name_color_ = Column('name_color', String(100))
-    name_back_color_ = Column('name_back_color', String(100))
+    custom_color_ = Column('custom_color', String(10000))
 
     @property
     def id(self):
@@ -77,16 +76,10 @@ class User(UserMixin, Base):
             ).group_by(cast(AcceptProblem.create_time, Date)).order_by(cast(AcceptProblem.create_time, Date)).all()]
 
     @property
-    def name_color(self):
-        if self.name_color_ is None:
+    def custom_color(self):
+        if self.custom_color_ is None:
             return None
-        return self.name_color_.split(',')
-
-    @property
-    def name_back_color(self):
-        if self. name_back_color_ is None:
-            return None
-        return self.name_back_color_.split(',')
+        return eval(self.custom_color_)
 
     def check_password(self, password):
         return self.password == password
