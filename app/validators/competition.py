3c064322862fad6c5fda9d7258dca0e22bca358f
Jiubei0408 <757795210@qq.com>
2021-02-16 09:50:45 +0800

    修改换行符格式

--- app/validators/competition.py
+++ app/validators/competition.py
@@ -1,1 +1,16 @@
-from wtforms import IntegerField, StringFieldfrom wtforms.validators import DataRequiredfrom app.validators.base import DateTimeFormclass CreateCompetitionForm(DateTimeForm):    name = StringField(validators=[DataRequired(message='competition name cannot be empty')])    link = StringField(validators=[DataRequired(message='competition link cannot be empty')])    remark = StringField()class ModifyCompetitionForm(DateTimeForm):    name = StringField()    link = StringField()    remark = StringField()
+from wtforms import IntegerField, StringField
+from wtforms.validators import DataRequired
+
+from app.validators.base import DateTimeForm
+
+
+class CreateCompetitionForm(DateTimeForm):
+    name = StringField(validators=[DataRequired(message='competition name cannot be empty')])
+    link = StringField(validators=[DataRequired(message='competition link cannot be empty')])
+    remark = StringField()
+
+
+class ModifyCompetitionForm(DateTimeForm):
+    name = StringField()
+    link = StringField()
+    remark = StringField()
