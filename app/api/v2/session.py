e7162898317ab80ab056937dd682d11ca8abbe27
Jiubei0408 <757795210@qq.com>
2021-12-02 20:46:56 +0800

    添加单点登录

--- app/api/v2/session.py
+++ app/api/v2/session.py
@@ -4,7 +4,7 @@ from flask_login import current_user, login_required, login_user, logout_user
 from app.libs.error_code import AuthFailed, DeleteSuccess, Success
 from app.libs.red_print import RedPrint
 from app.models.user import User
-from app.validators.session import LoginForm
+from app.validators.session import LoginForm, SSOLoginForm
 
 api = RedPrint('session')
 
@@ -29,6 +29,32 @@ def create_session_api():
     if not user.check_password(form['password']):
         raise AuthFailed('Wrong username or password')
     login_user(user, remember=True)
+    raise Success('Login successful')
+
+
+@api.route("/ssoLogin", methods=['POST'])
+def create_session_sso_api():
+    form = SSOLoginForm().validate_for_api().data_
+    username = form['username']
+    ticket = form['ticket']
+    import requests
+    check_url = 'https://api.zuccacm.top/sso/v1/ticket/check'
+    resp = requests.post(url=check_url, json={
+        'username': username,
+        'ticket': ticket
+    })
+    if resp.status_code != 200:
+        raise AuthFailed('验证失败')
+    user = User.get_by_id(form['username'])
+    if user is None:
+        import random
+        from string import digits, ascii_letters
+        password = ''.join(random.sample(ascii_letters + digits, 20))
+        user = User.create(
+            username=username, nickname=username,
+            password=password, group='未分组'
+        )
+    login_user(user)
     raise Success('Login successful')
 
 
--- app/validators/session.py
+++ app/validators/session.py
@@ -7,3 +7,8 @@ from app.validators.base import BaseForm
 class LoginForm(BaseForm):
     username = StringField(validators=[DataRequired(message='Username cannot be empty')])
     password = StringField(validators=[DataRequired(message='Password cannot be empty')])
+
+
+class SSOLoginForm(BaseForm):
+    username = StringField(validators=[DataRequired(message='Username cannot be empty')])
+    ticket = StringField(validators=[DataRequired(message='Ticket cannot be empty')])
